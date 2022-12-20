8ba8be042fcb61b3cbb3f71ab498fc8db879ea5b
Jeff Trojan <j.dauda@alustudent.com>
2022-12-20 19:29:06 +0200

    Templated index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const express = require('express')
 const Gun = require('gun')
+const app = express()
+const port = 8080
+
+app.use(Gun.serve);
+
+const server = app.listen(port, () => {
+    console.log(`Webchat app is listening at http://localhost:${port}`)
+
+})
+
+Gun({web: server})
